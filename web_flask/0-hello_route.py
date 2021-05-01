4eca3fd1382865e1099ba4f9d138386382ccb149
jdanielue <jdurregoe@gmail.com>
2021-05-01 16:44:44 -0500

    fix the bug

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """ script to create a web app """
-
-
 from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.r("/")
-def router():
+@app.route("/")
+def r():
     """ return value """
     return "Hello HBNB!"
 
