2fadf08223866c24c1759b322073f02cde8b65ed
jdanielue <jdurregoe@gmail.com>
2021-05-02 16:18:54 -0500

    s4

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,4 +64,5 @@ class FileStorage:
             return
         else:
             key = obj.__class__.__name__+"."+obj.id
-            del self.__objects[key]
+            if (key in self.__objects):
+                del self.__objects[key]
